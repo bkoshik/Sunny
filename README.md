293ab58c71057bdedae490f3c590b10df0f4210a
bkoshik <bakubakosh@gmail.com>
2025-04-27 22:39:54 +0500

    Updaaate!

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Sunny
 Sunny is a weather fetching CLI based on [thundery](https://github.com/loefey/thundery) and [rainy](https://github.com/liveslol/rainy)
+
+```Bash
+sunny -c York
+```
+
+![York weather](img/full_York.png)
+
+
+```Bash
+sunny -ac York
+```
+
+![Only Ascii art](img/only_art_York.png)
+
+```Bash
+sunny -u imperial -c York -f "{region}{\n}{temperature}"
+```
+
+![Formatted York weather](img/formatted_York.png)
--- src/modules/format.py
+++ src/modules/format.py
@@ -1,28 +1,36 @@
 # ========== IMPORT ==========
 import argparse
 import re
-from typing import NamedTuple
 from parser import configure_parser
-from fetch_weather import fetch_weather_wttr, fetch_weather_owm
+from fetch_weather import fetch_weather_wttr, fetch_weather_owm, fetch_weather_wapi
+from config import api_owm, api_wapi
 
 # ========== FORMAT ==========
+
+
 def print_format(type: str) -> str:
     args: argparse.Namespace = configure_parser()
-    weather: NamedTuple = fetch_weather_owm(args.City, args.Units)
+    if args.Source == "wttr.in":
+        weather = fetch_weather_wttr(args.City, args.Units)
+    elif args.Source == "openweathermap":
+        weather = fetch_weather_owm(args.City, args.Units, api_owm)
+    elif args.Source == "weatherapi":
+        weather = fetch_weather_wapi(args.City, args.Units, api_wapi)
 
     format: dict[str, str] = {
-        "reg": weather.region,
-        "desc": weather.description,
-        "temp": weather.temperature,
+        "region": weather.region,
+        "description": weather.description,
+        "temperature": weather.temperature,
         "wind": weather.wind,
-        "srise": weather.sunrise,
-        "sset": weather.sunset,
+        "sunrise": weather.sunrise,
+        "sunset": weather.sunset,
         "date": weather.timestamp,
         "\\n": "\n",
         "\\t": "\t",
     }
 
     return type.format(**format)
+
 
 def json_template_render(template: str, data: dict) -> str:
     def extractor(match):
